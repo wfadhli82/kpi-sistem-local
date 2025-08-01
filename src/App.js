70e277d771f9503459375f1327c3a52c6b31aefb
wfadhli82 <wfadhli82@gmail.com>
2025-08-01 15:12:59 +0800

    fix react router github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import UserInterface from "./UserInterface";
 import * as XLSX from "xlsx";
